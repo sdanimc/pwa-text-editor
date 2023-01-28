3115708bc7d4f137335f669bfa8a165ac8bad778
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-01-28 10:08:11 -0800

    coded all client/src/js todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.put({ id: 1, value: content});
+  const result = await request;
+  console.log('Database updated', result.value);
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  result 
+  ? console.log('All from Db:', result) 
+  : console.log('Error, Database is empty');
+  return result?.value
+}
 
 initdb();
